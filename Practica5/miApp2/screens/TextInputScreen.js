d0c6edbe8e9e4d2cb0714fef762bcb3b79a12701
DEFGEM <defgem@email.com>
2026-06-23 20:10:36 -0600

    SafeAreaView & ScrollView

--- Practica5/miApp2/screens/TextInputScreen.js
+++ Practica5/miApp2/screens/TextInputScreen.js
@@ -1,132 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  TextInput,
-  Alert,
-  Button,
-  StyleSheet,
-  Platform,
-} from "react-native";
+// zona1: importaciones de componetes y archivos
 
-if (Platform.OS === "web") {
-  Alert.alert = (titular, mensaje, boton) => {
-    const list = Array.isArray(mensaje) ? mensaje : boton;
-    if (list) {
-      if (window.confirm(titular)) list.find((b) => b.onPress)?.onPress();
-    } else {
-      window.alert(titular + (mensaje ? "\n" + mensaje : ""));
-    }
-  };
-}
+import { StatusBar } from "expo-status-bar"; //puede ser opcion quitarlo o no
+import { StyleSheet, Text, View } from "react-native";
 
+// zona2 main }: hofar de los componetes
 export default function TextInputScreen() {
-  const [nombre, setNombre] = useState();
-  const [correo, setCorreo] = useState();
-  const [contraseña, setContraseña] = useState();
-  const [numero, setNumero] = useState();
-  const [bio, setBio] = useState();
-
-  const registro = () => {
-    if (!nombre || !correo || !contraseña || !numero) {
-      Alert.alert("Faltan datos", "Completa tos slos campos");
-      return;
-    }
-
-    if (!correo.includes("@") || !correo.includes(".com")) {
-      Alert.alert("Correo inválido", "El correo debe contener @ y .com");
-      return;
-    }
-
-    if (contraseña.length < 6) {
-      Alert.alert("Contraseña invalida", "Minimo 6 caracteres");
-      return;
-    }
-
-    if (!numero.match(/^[0-9+ ]+$/)) {
-      Alert.alert("Numero invalido", "El numero debe contener solo numeros");
-      setNumero("");
-      return;
-    }
-
-    Alert.alert(`Registrar ${nombre}`, [
-      {
-        text: "No",
-        style: "calcel",
-      },
-      {
-        text: "Si",
-        onPress: () => {
-          Alert.alert("Exito", `Usuario registrado con exito`);
-        },
-      },
-    ]);
-  };
-
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.input}>
-        <Text style={styles.Titulo}>Formulario de registro de usuario</Text>
-        {/* nombre del usuario */}
-        <TextInput
-          style={styles.input}
-          placeholder="Ingrese su nombre"
-          placeholderTextColor="#999"
-          autoCapitalize="words"
-          value={nombre}
-          onChangeText={(texto) => setNombre(texto)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Ingrese tu correo electronico"
-          placeholderTextColor="#999"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          value={correo}
-          onChangeText={(texto) => setCorreo(texto)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Ingrese tu contraseña minimo 6 caracteres"
-          placeholderTextColor="#999"
-          secureTextEntry={true}
-          value={contraseña}
-          onChangeText={(texto) => setContraseña(texto)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="ingresa tu numero de telefono "
-          placeholderTextColor="#999"
-          keyboardType="number-pad"
-          maxLength={12}
-          value={numero}
-          onChangeText={(texto) => setNumero(texto)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Sobre ti (opcional)"
-          placeholderTextColor="#999"
-          multiline={true}
-          numberOfLines={4}
-          maxLength={20}
-          value={bio}
-          onChangeText={(texto) => setBio(texto)}
-        />
-
-        <Button title="Registrar" onPress={registro} color="#00ff00" />
-
-        <StatusBar style="auto" />
-      </View>
-    </ScrollView>
+    <View style={styles.input}>
+      <Text>aqui va la practica de CAG</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+// Zona 3 Estilos y Posicionamiento
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "z#fff",
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "column",
