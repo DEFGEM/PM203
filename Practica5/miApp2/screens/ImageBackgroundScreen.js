048b669b6e823edc37e3b1a84b2a8f9c0f55df80
DEFGEM <defgem@email.com>
2026-07-01 15:03:51 -0600

    ImageBackgroung & SlapshScreen:

--- Practica5/miApp2/screens/ImageBackgroundScreen.js
+++ Practica5/miApp2/screens/ImageBackgroundScreen.js
@@ -1,24 +1,93 @@
-// zona1: importaciones de componetes y archivos
+/* Zona 1: Importanciones de componentes y archivos */
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Button,
+  Image,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 
-import { StatusBar } from "expo-status-bar"; //puede ser opcion quitarlo o no
-import { StyleSheet, Text, View, Image } from "react-native";
-
-// zona2 main }: hofar de los componetes
+/* Zona 2: Main - Hogar de los componentes */
 export default function ImageBackgroundScreen() {
+  const [fondo, setFondo] = useState(require("../assets/fondo.jpg.jpeg"));
+
+  const [splash, setSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplash(false);
+    }, 3000);
+  }, []);
+
+  if (splash) {
+    return (
+      <View style={styles.splashContainer}>
+        <Image
+          source={require("../assets/wave.png")}
+          resizeMode="contain"
+          style={styles.logo}
+        />
+
+        <Text style={styles.carga}> Cargando...</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const cambiarFondo = () => {
+    setFondo(
+      fondo === require("../assets/fondo.jpg.jpeg")
+        ? require("../assets/fondo2.jpg.jpeg")
+        : require("../assets/fondo.jpg.jpeg"),
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>aquie va la practica de Ines</Text>
-    </View>
+    <ImageBackground source={fondo} style={styles.fondo} resizeMode="contain">
+      <View style={styles.centro}>
+        <Text style={styles.texto}>Bienvenido</Text>
+        <Button title="Cambiar Fondo" onPress={cambiarFondo} />
+      </View>
+      <StatusBar style="auto" />
+    </ImageBackground>
   );
 }
 
-// Zona 3 Estilos y Posicionamiento
+/* Zona3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-  container: {
+  fondo: {
     flex: 1,
-    backgroundColor: "#fff",
+    width: "100%",
+  },
+  centro: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column",
+  },
+  texto: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+  },
+  splashContainer: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#1E88E5",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  carga: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 15,
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 });
