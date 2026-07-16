5962226bbbdfe1a9912d943b43168c142b2b184e
DEFGEM <defgem@email.com>
2026-07-15 18:48:44 -0600

    Expo Router

---
+++ usuarioApi/app/_loyaut.js
@@ -0,0 +1,9 @@
+// _loyaut.js === sintasisis de na vabegacion (reservadaa configuraciones )
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    // ya tenemso una navegacion en pila
+    <Stack screenOptions={{ headerShown: false }}/>
+  )
+}
